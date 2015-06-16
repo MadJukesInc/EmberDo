7d4830902416a0af8070e4747b40a9379aad3047
pineauk <kpineau@upei.ca>
2015-06-16 16:27:13 -0300

    feat(users): setup the users model and controllers

--- client/app/controllers/users.js
+++ client/app/controllers/users.js
@@ -3,16 +3,14 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   editUsers: false,
 
-  users: model.findAll(),
-
   actions: {
     deleteUser: function (user) {
-      this.model.delete(user);
+      //this.model.delete(user);
     },
     addUser: function () {
-      var newUser = {};
-
-      this.model.create(newUser);
+      //var newUser = {};
+      //
+      //this.model.create(newUser);
     }
   }
 });
--- client/app/routes/users.js
+++ client/app/routes/users.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function (controller) {
     controller.set('title', 'User List');
+    controller.set('users', this.store.find('user'));
+
+  },
+  model: function () {
+    return this.store.find('user');
   }
 });
