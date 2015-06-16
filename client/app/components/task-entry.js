13fe88d6f4e6e76885fc6fe6aa175b008a35e73e
pineauk <kpineau@upei.ca>
2015-06-16 09:39:11 -0300

    feat(task-heleprs): add js to task views

--- client/app/components/task-entry.js
+++ client/app/components/task-entry.js
@@ -1,4 +1,52 @@
 import Ember from 'ember';
 
+
 export default Ember.Component.extend({
-});
+  tagName: 'tr',
+  addingMember: false,
+  editingTitle: false,
+
+  isOwner: function () {
+    return this.get('task.owner') === 1;
+  },
+  //users: store.find('user'),
+  //filteredUsers: _.difference(users, task.members),
+
+  actions: {
+    toggleAddMember: function () {
+      this.set('addingMember', true);
+    }
+    ,
+    toggleTitleEdit: function () {
+      this.set('editingTitle', true);
+    }
+    ,
+    toggleCompleted: function (task) {
+      task.completed = true;
+      this.model.update(task.id, task);
+    }
+    ,
+    addMember: function (task, member) {
+      console.log(task);
+      console.log(member);
+      this.set('addingMember', false);
+    }
+    ,
+    deleteTask: function (task) {
+      this.store.deleteRecord(task);
+    }
+    ,
+    removeMember: function (task, member) {
+      var members = _.difference(task.members, member);
+      //task.members = members;
+      this.set('task.members', members);
+    }
+    ,
+    updateTitle: function (title) {
+      //task.set('title', title);
+      //this.store.push('task', this.store.normalize('task', task));
+      this.set('editingTitle', false);
+    }
+  }
+})
+;
