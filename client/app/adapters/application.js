75a7293c59526d429bb76775fa01e98fcb8d2f8f
pineauk <kpineau@upei.ca>
2015-06-16 16:30:00 -0300

    feat(adapter): connect the models to REST kraken

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
-//export default DS.RESTAdapter.extend({
-//  namespace: 'api',
-//  host: 'http://localhost:8080'
-//});
+export default DS.RESTAdapter.extend({
+  namespace: 'api',
+  host: 'http://localhost:8088'
+});
 
-export default DS.FixtureAdapter;
+//export default DS.FixtureAdapter;
