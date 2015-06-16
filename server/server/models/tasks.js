28c2ba7486578cce3dd604a6617e716d2d900808
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2015-06-16 14:55:49 -0300

    feat(models): public models to access database
    These models provide public methods to access database. These modules
    should be required and used when there is a necessity to access the db.
    
    These files are almost direct imports from corresponding files of
    https://github.com/MadJukesInc/CthulDo project.

---
+++ server/server/models/tasks.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var _ = require('lodash');
+var B = require('bluebird');
+
+var Tasks = require('./Tasks');
+
+/**
+ * Function returns map to work with {@link Tasks} table
+ * @returns {{get: Function, post: Function, put: Function, delete: Function}}
+ * @constructor
+ */
+var TasksModel = function TasksModelConstructor() {
+    return {
+        /**
+         * Find tasks and execute callback
+         * @param {Number} [id] ID of the task
+         * @param user
+         * @param {Function} cb Callback to execute after we found tasks
+         * @returns {*}
+         */
+        get: function (id,user, cb) {
+            var found;
+
+            if (_.isUndefined(cb) && _.isFunction(user)) {
+                cb = user;
+                user = id;
+                id = null;
+            }
+
+            if (_.isNull(id)) {
+                //found = Tasks.findAll({where: {$or: [{owner: user.id},{ members: {$contains: [user.username]}}]}});
+                found = Tasks.findAll();
+            }
+            else {
+                found = Tasks.findById(id);
+            }
+
+            return found
+                .then(function (results) {
+                    cb(null, results);
+                })
+                .catch(function (err) {
+                    cb(err);
+                });
+        },
+        post: function (task, cb) {
+            var promises = [];
+            var fulfilledPromise;
+
+            if (_.isArray(task)) {
+                _.forEach(user, function(v) {
+                    promises.push(Tasks.create(v));
+                });
+                fulfilledPromise = B.all(promises);
+            }
+            else if (_.isPlainObject(task)) {
+                fulfilledPromise = Tasks.create(task);
+            }
+
+            return fulfilledPromise
+                .then(function (result) {
+                    cb(null, result);
+                })
+                .catch(function (err) {
+                    cb(err);
+                });
+        },
+        put: function (id, task, cb) {
+            return Tasks.update(task, { where: { id: id } })
+                .then(function (affected) {
+                    cb(null, { 'affected': affected[0] });
+                })
+                .catch(function (err) {
+                    cb(err);
+                });
+        },
+        delete: function (id, cb) {
+            return Tasks.destroy({ where: { id: id } })
+                .then(function (affected) {
+                    cb(null, { 'affected': affected });
+                })
+                .catch(function (err) {
+                    cb(err);
+                });
+        }
+    }
+};
+
+
+module.exports = TasksModel();
---
+++ server/server/models/users.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var _ = require('lodash');
+var B = require('bluebird');
+
+var Users = require('./Users');
+
+var UsersModel = function UsersModelConstructor() {
+    return {
+        get: function (username, cb) {
+            var found;
+            //var username = usernameString + '';
+            //console.log(username);
+            if (_.isUndefined(cb) && _.isFunction(username)) {
+                cb = username;
+                username = null;
+            }
+
+            if (_.isNull(username)) {
+                found = Users.all();
+            }
+            else {
+                if (_.isString(username)) {
+                    found = Users.findOne({where: {username: username}});
+                }
+                else {
+                    found = Users.findById(username);
+                }
+            }
+
+            return found
+                .then(function (result) {
+                    cb(null, result);
+                })
+                .catch(function (err) {
+                    cb(err);
+                });
+        },
+        post: function (user, cb) {
+            var promises = [];
+            var fulfilledPromise;
+
+            if (_.isArray(user)) {
+                _.forEach(user, function (v) {
+                    promises.push(Users.create(v));
+                });
+                fulfilledPromise = B.all(promises);
+            }
+            else if (_.isPlainObject(user)) {
+                fulfilledPromise = Users.create(user);
+            }
+
+            return fulfilledPromise
+                .then(function (result) {
+                    cb(null, result);
+                })
+                .catch(function (err) {
+                    cb(err);
+                });
+        }
+    };
+};
+
+module.exports = UsersModel();
