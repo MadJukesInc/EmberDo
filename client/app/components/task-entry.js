3679d0663a1560e455a16376957955b39a68207a
pineauk <kpineau@upei.ca>
2015-06-16 10:26:37 -0300

    feat(task-entry): setup the events on the entry

--- client/app/components/task-entry.js
+++ client/app/components/task-entry.js
@@ -5,46 +5,35 @@ export default Ember.Component.extend({
   tagName: 'tr',
   addingMember: false,
   editingTitle: false,
+  selectedMember: '',
 
   isOwner: function () {
     return this.get('task.owner') === 1;
   },
-  //users: store.find('user'),
-  //filteredUsers: _.difference(users, task.members),
 
   actions: {
     toggleAddMember: function () {
       this.set('addingMember', true);
-    }
-    ,
+    },
     toggleTitleEdit: function () {
       this.set('editingTitle', true);
-    }
-    ,
+    },
     toggleCompleted: function (task) {
-      task.completed = true;
-      this.model.update(task.id, task);
-    }
-    ,
-    addMember: function (task, member) {
-      console.log(task);
-      console.log(member);
+      var completed = !task.completed;
+      this.set('task.completed', completed).save();
+    },
+    addMember: function (task) {
+      this.get('task.members').addObject(this.get('selectedMember')).save();
       this.set('addingMember', false);
-    }
-    ,
+    },
     deleteTask: function (task) {
       this.store.deleteRecord(task);
-    }
-    ,
+    },
     removeMember: function (task, member) {
-      var members = _.difference(task.members, member);
-      //task.members = members;
-      this.set('task.members', members);
-    }
-    ,
-    updateTitle: function (title) {
-      //task.set('title', title);
-      //this.store.push('task', this.store.normalize('task', task));
+      this.get('task.members').removeObject(member).save();
+    },
+    updateTitle: function (task) {
+      task.save();
       this.set('editingTitle', false);
     }
   }
