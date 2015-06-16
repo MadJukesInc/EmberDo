7dd64343caa85c79e36015d945f2401b8fa365a6
pineauk <kpineau@upei.ca>
2015-06-16 10:26:15 -0300

    feat(models): setup relations properly

--- client/app/models/task.js
+++ client/app/models/task.js
@@ -2,13 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  completed: DS.attr('bool'),
-  members: DS.attr('array'),
-  owner: DS.attr('int')
+  completed: DS.attr('boolean'),
+  members: DS.hasMany('user'),
+  owner: DS.belongsTo('user')
 });
 
 DS.Model.reopenClass({
   FIXTURES: [
-    {id: 1, title: 'test 1', completed: false, members: ['kyle'], owner: 1}
+    {id: 1, title: 'test 1', completed: false, members: [1], owner: 1}
   ]
 });
