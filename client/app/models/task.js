4aecf938f5f71ab46f037952a52def9c83583a0d
pineauk <kpineau@upei.ca>
2015-06-16 09:13:28 -0300

    feat(models): enable mock models

--- client/app/models/task.js
+++ client/app/models/task.js
@@ -1,9 +1,14 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  id: DS.attr('number'),
   title: DS.attr('string'),
   completed: DS.attr('bool'),
   members: DS.attr('array'),
   owner: DS.attr('int')
+});
+
+DS.Model.reopenClass({
+  FIXTURES: [
+    {id: 1, title: 'test 1', completed: false, members: ['kyle'], owner: 1}
+  ]
 });
--- client/app/models/user.js
+++ client/app/models/user.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  id: DS.attr('number'),
   username: DS.attr('string'),
   password: DS.attr('string'),
   role: DS.attr('string')
+});
+
+DS.Model.reopenClass({
+  FIXTURES: [
+    {id: 1, username: 'pineauk', password: 'blah', role:'admin'}
+  ]
 });
