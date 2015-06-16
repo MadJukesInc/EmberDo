61b428b969a4e464d63035ecdf9df7c09a1209b2
Jason Würtz <jwuertz@upei.ca>
2015-06-16 11:24:21 -0300

    fix(application.js): remove old 404 code

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -1,14 +1,5 @@
 export default Ember.Route.extend({
   actions: {
-    error: function onError(error, transition) {
 
-      if (error && error.status === 400) {
-        // error substate and parent routes do not handle this error
-        return this.transitionTo('modelNotFound');
-      }
-
-      // Return true to bubble this event to any parent route.
-      return true;
-    }
   }
 });
