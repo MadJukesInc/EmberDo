66cb730ddd5cf8e5e958e955c25761a6501f1491
pineauk <kpineau@upei.ca>
2015-06-16 16:29:02 -0300

    feat(sign-in.js): remove the sign-in controller

--- client/app/controllers/sign-in.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  actions: {
-    login: function () {
-      //TODO create the login function to make rest call, perhaps encrypt password
-    }
-  }
-});
