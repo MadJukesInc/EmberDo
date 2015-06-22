66b11e2e2e4039e57fb11c39c34c6cd445153d1f
pineauk <kpineau@upei.ca>
2015-06-22 09:27:55 -0300

    chore(login.js): disable the login function

--- client/app/controllers/login.js
+++ client/app/controllers/login.js
@@ -10,16 +10,16 @@ export default Ember.Controller.extend({
   },
   actions: {
     authenticate: function () {
-      var user = this.get('userLogin').getProperties('username', 'password');
-      var self = this;
-      Ember.$.post('http://localhost:8088/external_login', user, function (result) {
-        //http://stackoverflow.com/questions/24963677/can-i-use-ember-simple-auth-with-express-passport-session-domain-cookies
-        //This is a magical link of goodness
-        if(result.passport.user) {
-          self.transitionTo('task-list');
-        }
-        console.log(result);
-      });
+      //var user = this.get('userLogin').getProperties('username', 'password');
+      //var self = this;
+      //Ember.$.post('http://localhost:8088/external_login', user, function (result) {
+      //  //http://stackoverflow.com/questions/24963677/can-i-use-ember-simple-auth-with-express-passport-session-domain-cookies
+      //  //This is a magical link of goodness
+      //  if(result.passport.user) {
+      //    self.transitionTo('task-list');
+      //  }
+      //  console.log(result);
+      //});
     }
   }
 })
