a12a523c1d989b6526d087f92b11efd77eaf18a5
pineauk <kpineau@upei.ca>
2015-06-23 23:29:12 -0300

    refactor(pods): more pods changes

---
+++ client/app/models/task.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  completed: DS.attr('boolean'),
+  members: DS.hasMany('user',{ async: true }),
+  owner: DS.belongsTo('user',{ async: true })
+});
+
+//DS.Model.reopenClass({
+//  FIXTURES: [
+//    {id: 1, title: 'test 1', completed: false, members: [1], owner: 1}
+//  ]
+//});
